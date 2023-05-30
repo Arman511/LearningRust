f8bcf15d74e154a0581422949468986675e8b19b
40392145 <40392145@ads.qub.ac.uk>
2023-05-30 19:34:34 +0100

    Restaurant sim

--- Restaurant_Simulator/target/debug/deps/Restaurant_Simulator.d
+++ Restaurant_Simulator/target/debug/deps/Restaurant_Simulator.d
@@ -1,6 +1,6 @@
-C:\Users\Arman\OneDrive\Documents\GitHub\LearningRust\Restaurant_Simulator\target\debug\deps\Restaurant_Simulator.exe: src\main.rs src\restaurants.rs
+C:\Users\Arman\OneDrive\Documents\GitHub\LearningRust\restaurant_simulator\target\debug\deps\restaurant_simulator.exe: src\main.rs src\restaurants.rs
 
-C:\Users\Arman\OneDrive\Documents\GitHub\LearningRust\Restaurant_Simulator\target\debug\deps\Restaurant_Simulator.d: src\main.rs src\restaurants.rs
+C:\Users\Arman\OneDrive\Documents\GitHub\LearningRust\restaurant_simulator\target\debug\deps\restaurant_simulator.d: src\main.rs src\restaurants.rs
 
 src\main.rs:
 src\restaurants.rs:
